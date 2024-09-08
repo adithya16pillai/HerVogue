aefd9c39cd0e28d5642efd2c7b96e4ae1a046e3e
Adithya Pillai <adithya.pillai16@gmail.com>
2024-09-08 21:39:16 +0530

    Co-authored-by: Tanisha Jauhari <Tanisboxed@users.noreply.github.com>

--- HerVogue/src/screens/Initial.js
+++ HerVogue/src/screens/Initial.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect} from "react";
 import { View, Text, ActivityIndicator } from "react-native";
 
-export default function Initial({ onLoad}) {
-    
+export default function Initial({ onLoadingComplete }) {
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+            onLoadingComplete;
+        },1000);
+        return () => clearTimeout(timer);
+    }, []);
 }
